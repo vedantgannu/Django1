294acf5d184cd42dffa28b9efda33dcf3b794932
VGan <vedantgannu@gmail.com>
2020-04-30 01:17:22 -0400

    URl routing

---
+++ django_project/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+#when /blog encountered in URL, website routes an empty string to the blog.urls.py file
+#looks for which url pattern matches empty route (''). Then routed to views.home
+
+#when /blog/about encountered in URL, "blog" is rerouted to this file. Then
+urlpatterns = [
+    path('', views.home, name = 'blog-home'),
+    path('about/', views.about, name = 'blog-about')
+
+]
